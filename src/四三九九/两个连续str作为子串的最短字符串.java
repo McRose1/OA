e9e2a95f42a52f0db425f7640ac9789ae4e6e6d4
Jiaye Zhu <jzhu50@ur.rochester.edu>
2020-04-23 22:09:17 -0400

    4.23

--- src/四三九九/两个连续str作为子串的最短字符串.java
+++ src/四三九九/两个连续str作为子串的最短字符串.java
@@ -1,6 +1,6 @@
 package 四三九九;
 
-/*  两个子串
+/*  两个连续str作为子串的最短字符串
     给定一个字符串s, 请计算输出含有连续两个s作为子串的最短字符串。 注意两个s可能有重叠部分。例如,"ababa"含有两个"aba".
 
     输入描述：
@@ -17,5 +17,40 @@ package 四三九九;
 
  */
 //  KMP
+import java.util.Scanner;
+
 public class 两个连续str作为子串的最短字符串 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int overlap = KMP(s);
+        String res = "";
+        res = s + s.substring(overlap);
+        System.out.println(res);
+    }
+
+    private static int KMP(String s) {
+        int[] pi = new int[s.length()];
+        pi[0] = 0;
+        // 前缀
+        int k = 0;
+        // 后缀
+        int j = 1;
+
+        while (j < s.length()) {
+            if (s.charAt(k) == s.charAt(j)) {
+                k++;
+                pi[j] = k;
+                j++;
+            } else {
+                if (k != 0) {
+                    k = pi[k - 1];
+                } else {
+                    pi[j] = k;
+                    j++;
+                }
+            }
+        }
+        return pi[s.length() - 1];
+    }
 }
