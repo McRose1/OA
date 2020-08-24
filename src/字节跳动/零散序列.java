ea932507a96e229d3ee516980c97dd4f1127e49e
Jiaye Zhu <jzhu50@ur.rochester.edu>
2020-08-24 00:02:05 -0400

    8.24

---
+++ src/字节跳动/零散序列.java
@@ -0,0 +1,40 @@
+package 字节跳动;
+
+/*  零散序列（LC 368）
+
+    示例:
+    输入：
+    4 3
+    1 2 3 4
+    输出：
+    10
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 零散序列 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        Arrays.sort(nums);
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+        
+    }
+}
